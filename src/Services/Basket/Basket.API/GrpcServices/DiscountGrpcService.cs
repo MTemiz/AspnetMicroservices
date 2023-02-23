2cd09b86bc59d2a1ed9d0064039f32f743d09e94
Mesut Temiz <mesut@Mesut-MBP.home>
2023-02-23 20:54:26 +0300

    Discount.Grpc servisinin tüketilmesi sağlandı.

--- src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -3,9 +3,9 @@ using Discount.Grpc.Protos;
 
 namespace Basket.API.GrpcServices
 {
-	public class DiscountGrpcService
-	{
-		private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
 
         public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
         {
@@ -18,6 +18,6 @@ namespace Basket.API.GrpcServices
 
             return await _discountProtoService.GetDiscountAsync(discountRequest);
         }
-	}
+    }
 }
 
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
-﻿using Basket.API.Repositories;
+﻿using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,8 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddScoped<DiscountGrpcService>();
 
 var app = builder.Build();
 
