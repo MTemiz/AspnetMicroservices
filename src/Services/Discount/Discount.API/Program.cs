fc34b7db3632f9007af8c8ce8965608403198f94
Mesut Temiz <mesut@Mesut-MBP.home>
2023-03-09 23:29:24 +0300

    docker compose ve repository register düzenlemesi.

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Discount.API.Extensions;
+using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
