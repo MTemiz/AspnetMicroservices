481d139ebbc17e32441881b2e23099390ca51874
Mesut Temiz <mesut@Mesut-MBP.home>
2023-06-05 21:38:14 +0300

    GetOrdersList feature geliştirmesi yapıldı.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            this._orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
+
--- src/Services/Ordering/Ordering.Application/Mappings/MappingPofile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingPofile.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingPofile : Profile
     {
-        
+        public MappingPofile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
 
