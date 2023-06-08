be67eae62a68c0af1cbc43e93b7b95376fcbb2ee
Mesut Temiz <mesut@Mesut-MBP.home>
2023-06-08 22:41:06 +0300

    UpdateOrder feature geliştirmesi yapıldı.

--- src/Services/Ordering/Ordering.Application/Mappings/MappingPofile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingPofile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 
@@ -12,6 +13,7 @@ namespace Ordering.Application.Mappings
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
             CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }
